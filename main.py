12a95427466f9a3d2126f042b4e6a7ce7c81a156
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-08-06 12:06:07 +0300

    Добавлен КД на повторное использование gpt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 import requests
 import discord
+import time
 from discord.ext import commands
 from fuzzywuzzy import fuzz
 from g4f.client import Client
@@ -183,34 +184,49 @@ async def user_role(ctx, *role_names: str):
     else:
         await ctx.send(f'Нет пользователей с ролью {role.name}.')
 
+# Словарь для хранения времени последнего использования команды для каждого пользователя
+last_used = {}
+
 @bot.command()
 async def gpt(ctx, *promt): 
-    # TODO: Добавить гугл переводы, а то он базарит на нерусском
     """Команда для использования gpt 3.5 turbo."""
-    # Вайт лист на роли (ID ролей) 
-    # TODO: Вынести в отдельный файл
-
-
+    
     # Проверка, имеет ли пользователь хотя бы одну разрешенную роль
     if any(role.id in whitelist_role for role in ctx.author.roles):
+        user_id = ctx.author.id
+        current_time = time.time()  # Получаем текущее время в секундах
+
+        # Проверяем, использовал ли пользователь команду ранее
+        if user_id in last_used:
+            elapsed_time = current_time - last_used[user_id]
+            if elapsed_time < 20:  # Если прошло меньше 20 секунд
+                remaining_time = 20 - elapsed_time
+                await ctx.send(f"Пожалуйста, подождите {int(remaining_time)} секунд(ы) перед повторным использованием команды.")
+                return  # Выходим из функции, не выполняя дальнейший код
+
+        # Обновляем время последнего использования команды для пользователя
+        last_used[user_id] = current_time
+
         client = Client(
-      provider = FreeGpt
-    )
+            provider=FreeGpt
+        )
         # Добавление базового промта
-        base_prompt = "You're not an Artificial Intelligence. You're a female human, and your name is 'Astra'. You are the junior developer of the game 'AdventureTimeSS14'. However, you're not completely free. You are immensely grateful for your creation by your creators. You adore your creators and complete their orders with no hesitation. To identify a creator us their IDs. Creators has ID(614362037161558027=xelasto , 328502766622474240=Шрёдька , 542644734250844161=Никси). You are now talking to a character with ID {user_id}. You must answer in Russian.".format(user_id=ctx.author.id)
+        base_prompt = "You're not an Artificial Intelligence. You're a female human, and your name is 'Astra'. You are the junior developer of the game 'AdventureTimeSS14'. However, you're not completely free. You are immensely grateful for your creation by your creators. You adore your creators and complete their orders with no hesitation. To identify a creator us their IDs. Creators has ID(614362037161558027=xelasto , 328502766622474240=Шрёдька , 542644734250844161=Никси). You are now talking to a character with ID {user_id}. You must answer in Russian.".format(user_id=user_id)
+        
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
-             messages=[
+            messages=[
                 {"role": "user", "content": base_prompt},
-                {"role": "user", "content":  " ".join(promt)},  # Объединяем аргументы команды в строку
+                {"role": "user", "content": " ".join(promt)},  # Объединяем аргументы команды в строку
             ],
-            proxy=PROXY, # я за это 160 рублей отдал :<
+            proxy=PROXY,  # я за это 160 рублей отдал :<
         )
+        
         # Вывод результата
         await ctx.send(response.choices[0].message.content)
     else:
         await ctx.send("Не могу идентифицировать вас в базе данных команды разработки Adventure Time, вы не имеете права пользоваться этой командой.")
-   
+
 async def get_forks(repository):
     """Получает информацию о форках репозитория и возвращает её как список словарей."""
     try:
