f707563c18eaa23aeac37632336869f1b2e47025
Nix Silvam <nixsilvam@icloud.com>
2024-08-18 19:37:58 +0300

    remove fucked up commands

--- events/on_message.py
+++ events/on_message.py
@@ -1,34 +1,35 @@
 import re
 
+import requests
 from fuzzywuzzy import fuzz
 
 from bot_init import bot
+from config import AUTHOR, GLOBAL_SESSION, REPOSITORIES
+from data import JsonData
 
 
 @bot.event
 async def on_message(message):
-    # Игнорируем сообщения от самого бота
     if message.author == bot.user:
         return
-    # Игнорируем команды бота
+    
     if message.content.startswith(bot.command_prefix):
         await bot.process_commands(message)
         return
 
-    if any(fuzz.token_sort_ratio(message.content.lower(), phrase) >= 70 for phrase in phrases):
-        await message.channel.send("Через неделю.")
-        return
+    # if any(fuzz.token_sort_ratio(message.content.lower(), phrase) >= 70 for phrase in phrases):
+    #     await message.channel.send("Через неделю.")
+    #     return
     
     if f"<@{bot.user.id}>" in message.content:
-        # Извлекаем текст без упоминания бота
         text_without_mention = message.content.replace(f"<@{bot.user.id}>", "").strip()
-        # Проверяем, содержит ли текст любую вариацию "обнимает"
-        for variation in hug_variations:
+        data = JsonData()
+        for variation in data.get_data['hug_variations']:
             if fuzz.token_sort_ratio(text_without_mention.lower(), variation) > 80:
                 await message.channel.send("*Обнимает в ответ.*")
-                break  # Прерываем цикл после отправки сообщения
+                break
     
-    # Ищем паттерн в сообщении
+    # Search for GitHub issue/PR pattern and respond with the appropriate link
     match = re.compile(r'\[(n|o)(\d+)\]').search(message.content)
     if match:
         repo_code, number = match.groups()
@@ -37,22 +38,26 @@ async def on_message(message):
             await message.channel.send(f'{link}')
 
 def check_github_issue_or_pr(repo_code, number):
-    """
-    Возвращает ссылку на GitHub issue или PR в зависимости от введенного кода репозитория (n или o).
-
-    Args:
-        repo_code: Код репозитория (n или o).
-        number: Номер issue или PR.
-
-    Returns:
-        Ссылка на GitHub issue или PR, если она найдена, иначе None.
-    """
-    repo_name = repositories.get(repo_code)
+    repo_name = REPOSITORIES.get(repo_code)
     if not repo_name:
         return None
 
-    base_url = f'https://github.com/{author}/{repo_name}'
-    issue_url = f'{base_url}/issues/{number}'
-    pr_url = f'{base_url}/pull/{number}'
-    
-    return f'[{repo_name} {number}]({pr_url})'
+    base_api_url = f'https://api.github.com/repos/{AUTHOR}/{repo_name}'
+    issue_url = f'{base_api_url}/issues/{number}'
+    pr_url = f'{base_api_url}/pulls/{number}'
+
+    try:
+        pr_response = GLOBAL_SESSION.get(pr_url)
+        if pr_response.status_code == 200:
+            pr_data = pr_response.json()
+            return f'[{repo_name} PR {number}]({pr_data["html_url"]})'
+
+        issue_response = GLOBAL_SESSION.get(issue_url)
+        if issue_response.status_code == 200:
+            issue_data = issue_response.json()
+            return f'[{repo_name} Issue {number}]({issue_data["html_url"]})'
+
+    except requests.RequestException as e:
+        print(f"Ошибка при проверке GitHub issue или PR: {e}")
+
+    return None
