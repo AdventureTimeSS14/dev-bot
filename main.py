70ea9e2f2df555d6bd5ff6478dbc948837b201d5
Nix Silvam <nixsilvam@icloud.com>
2024-08-23 20:21:31 +0300

    fix imports

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from bot_init import bot
+from commands import (echo_command, gpt_command, help_command, ping_command,
+                      user_role_command)
+from commands.github import (achang_command, forks_command, github_processor,
+                             milestones_command, review_command)
 from config import DISCORD_KEY
+from events import on_message, on_ready
 
 if __name__ == '__main__':
     bot.run(DISCORD_KEY)
