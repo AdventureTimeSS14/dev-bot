ef84d7e7ad1fe2eab9774fe181d07e6f83c34c2c
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-08-31 00:21:51 +0300

    Правки

--- commands/github/git_fetch_pull.py
+++ commands/github/git_fetch_pull.py
@@ -13,29 +13,29 @@ async def fetch_merged_pull_requests():
         'Authorization': f'token {GITHUB}',
         'Accept': 'application/vnd.github.v3+json',
     }
-    
+
     last_checked_time = datetime.utcnow()
 
-    response = requests.get(f'https://api.github.com/repos/{AUTHOR}/{REPOSITORIES["n"]}/pulls?state=closed', headers=headers)
+    response = requests.get(
+        f'https://api.github.com/repos/{AUTHOR}/{REPOSITORIES["n"]}/pulls?state=closed', 
+        headers=headers
+    )
+
     if response.status_code == 200:
         pull_requests = response.json()
         for pr in pull_requests:
             if pr['merged_at']:
                 merged_at = datetime.strptime(pr['merged_at'], '%Y-%m-%dT%H:%M:%SZ')
-                if merged_at > last_checked_time:  # Проверяем, был ли PR замержен после последней проверки
+                if merged_at > last_checked_time:
                     pr_title = pr['title']
                     pr_url = pr['html_url']
                     description = pr.get('body', "Нет описания") 
                     author_name = pr['user']['login']
                     
-                    # Удаляем HTML-комментарии из описания
                     description = re.sub(r'<!--.*?-->', '', description, flags=re.DOTALL).strip()
-                    
-                    # Ищем :cl: и все, что идет после него
                     match = re.search(r'(:cl:.*?)(\n|$)', description, re.DOTALL)
                     if match:
                         cl_text = match.group(1).strip()
-                        
                         remaining_lines = description[match.end():].strip()
                         
                         if remaining_lines:
