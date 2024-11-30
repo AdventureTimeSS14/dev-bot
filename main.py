043027ee5be79480733199d2b067965f4d8fa5c0
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-12-01 00:19:58 +0300

    Добавил в мейн

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from commands.dbCommand import help_command, info_command, status_command
 from commands.github import (achang_command, forks_command, github_processor,
                              milestones_command, pr_changelog_send,
                              review_command)
+from commands.adt_team import (add_role_command, add_vacation_command, new_team_command,
+                               remove_role_command, remove_team_command, tweak_team_command)
 from config import DISCORD_KEY
 from events import on_command, on_error, on_message, on_ready
 
