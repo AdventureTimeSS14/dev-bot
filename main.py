0b4c3167a53ed20bbaf6630611ead757b745d50f
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-12-01 18:26:17 +0300

    правки опять

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from commands import (echo_command, gpt_command, help_command,
 from commands.adt_team import (add_role_command, add_vacation_command,
                                new_team_command, remove_role_command,
                                remove_team_command, team_help_command,
-                               tweak_team_command)
+                               tweak_team_command, end_vacation_command)
 from commands.dbCommand import help_command, info_command, status_command
 from commands.github import (achang_command, forks_command, github_processor,
                              milestones_command, pr_changelog_send,
