e5109e7c9ed438456169caeac717fe49f6d9a262
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-01-03 14:36:18 +0300

    🚀 Добавлены команды для перезапуска и обновления серверов SS14 (#40)
    * команда &restart для выключения бота переименована в &shutdows
    добавлено несколько новых команда для создания пост запросов к серверам ss14
    restart
    и
    update
    * Правка PyLinter
    * Правки

--- commands/status_command.py
+++ commands/status_command.py
@@ -3,21 +3,34 @@
 """
 
 from bot_init import bot
-from config import SS14_ADDRESS
+from config import SS14_ADDRESS, SS14_ADDRESS_DEV
 from events.update_status import (
     create_status_embed,
     get_ss14_server_status_second,
 )
 
-
+# Команда для получения статуса
 @bot.command(name="status")
-async def get_status_command(ctx):
+async def get_status_command(ctx, server_name: str = None):
     """
     Команда для получения статуса сервера SS14 в чате Discord.
+    Можно указать сервер, например: &status dev или &status mrp.
     """
     try:
-        # Получаем данные статуса с сервера
-        status_data = await get_ss14_server_status_second(SS14_ADDRESS)
+        # Установка адреса по умолчанию (если сервер не указан)
+        if server_name is None:
+            address = SS14_ADDRESS  # Основной адрес по умолчанию
+        elif server_name.lower() == "dev":
+            address = SS14_ADDRESS_DEV  # Адрес для dev сервера
+        elif server_name.lower() == "mrp":
+            address = SS14_ADDRESS  # Адрес для mrp сервера
+        else:
+            await ctx.send("❌ Некорректное имя сервера. Используйте 'dev' или 'mrp'.")
+            return
+
+        # Получаем данные статуса с выбранного сервера
+        print(f"Попытка подключения к серверу: {address}")
+        status_data = await get_ss14_server_status_second(address)
 
         # Если данные не получены, уведомляем пользователя
         if not status_data:
@@ -25,7 +38,7 @@ async def get_status_command(ctx):
             return
 
         # Создаем Embed сообщение с использованием данных
-        embed = create_status_embed(SS14_ADDRESS, status_data, ctx.author)
+        embed = create_status_embed(address, status_data, ctx.author)
         await ctx.send(embed=embed)
 
     except Exception as e:
