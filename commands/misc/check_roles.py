5169cd158545049dcf79e3105c1549f770b8f999
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-01-21 20:13:12 +0300

    Правки

--- commands/misc/check_roles.py
+++ commands/misc/check_roles.py
@@ -1,17 +1,20 @@
 from disnake.ext import commands
 
 
-def has_any_role_by_id(role_ids):
+def has_any_role_by_id(*role_id_groups):
     """
-    Декоратор для проверки, имеет ли пользователь одну из указанных ролей по их ID.
+    Декоратор для проверки, имеет ли пользователь одну из указанных ролей из нескольких групп ролей.
 
-    :param role_ids: Список ID ролей, доступ к которым проверяется.
+    :param role_id_groups: Список массивов ID ролей, доступ к которым проверяется.
     :return: Декоратор команды.
     """
-
     async def predicate(ctx):
-        # Проверяем наличие хотя бы одной из указанных ролей у пользователя
-        has_role = any(role.id in role_ids for role in ctx.author.roles)
+        # Проверяем, есть ли у пользователя хотя бы одна роль из каждого массива ролей
+        has_role = False
+        for role_ids in role_id_groups:
+            if any(role.id in role_ids for role in ctx.author.roles):
+                has_role = True
+                break  # Прерываем, как только нашли хотя бы одну подходящую роль
         if not has_role:
             await ctx.send("❌ У вас нет доступа к этой команде.")
         return has_role
